26d9401cac09b8c544ca58a8adcdecfffa6c8bcb
Erick Zanardo <erickzanardoo@gmail.com>
2025-01-23 14:53:09 -0300

    feat: using shared preferences instead of a file for the api key

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:stork_hub/app/app.dart';
 import 'package:stork_hub/bootstrap.dart';
 import 'package:stork_hub/environment/app_environment.dart';
@@ -8,9 +7,7 @@ import 'package:stork_hub/repositories/api_key_repository.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final directory = await getApplicationSupportDirectory();
-
-  final apiKeyRepository = ApiKeyRepository(directory: directory.path);
+  final apiKeyRepository = ApiKeyRepository();
 
   const environment = AppEnvironment(
     storkUrl: 'http://localhost:8787',
