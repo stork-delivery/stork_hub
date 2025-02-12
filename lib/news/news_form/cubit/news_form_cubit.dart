ef9b6a093c55086d70b7e1fc22606d85eeb8b0dc
Erick Zanardo <erickzanardoo@gmail.com>
2025-02-12 10:32:24 -0300

    lint

--- lib/news/news_form/cubit/news_form_cubit.dart
+++ lib/news/news_form/cubit/news_form_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stork_hub/models/models.dart';
+import 'package:stork_hub/news/news_form/cubit/news_form_state.dart';
 import 'package:stork_hub/repositories/stork_repository.dart';
-import 'news_form_state.dart';
 
 class NewsFormCubit extends Cubit<NewsFormState> {
   NewsFormCubit({
